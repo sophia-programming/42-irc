525669e51698bfe6a699c1a029a30a3db7bca9e5
oaoba <hacchi0803.rera1126@gmail.com>
2024-06-01 19:55:18 +0900

    #17: [Add] Welcomeメッセージ

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -5,6 +5,13 @@
 #include "Message.hpp"
 
 class Server;
+
+// Welcomeメッセージ(001 ~ 004)
+#define RPL_WELCOME(nick) ":ft_irc 001 " + nick + " :Welcome to the Internet Relay Chat Network " + nick + "\r\n"
+#define RPL_YOURHOST(nick) ":ft_irc 002 " + nick + " :Your host is ft_irc, running version 1.0\r\n"
+#define RPL_CREATED(nick) ":ft_irc 003 " + nick + " :This server was created in C++ [Sunday, June 16, 2024]\r\n"
+#define RPL_MYINFO(nick) ":ft_irc 004 " + nick + " :FT_IRC 1.0 contributes: oaoba yushimom stakimot \r\n"
+
 // エラーメッセージ
 #define ERR_UNKNOWNCOMMAND(nick, command) "421 " + nick + " " + command + " :Unknown command\r\n"
 #define ERR_ERRONEUSNICKNAME(nick, nickname) "432 " + nick + " " + nickname + " :Erroneus nickname\r\n"
@@ -15,5 +22,6 @@ class Server;
 void PASS(Client &client, Server *server, const Message &message);
 void NICK(Client &client, std::map<std::string, int> &map_nick_fd, const Message &message);
 void SendMessage(int fd, const std::string &message, int flag);
+void SendWelcomeMessage(const Client &client);
 
 #endif
