64feb63a6eca167f29154aabfe1214dd2003a14f
shhio08 <kiiro18.sh@gmail.com>
2024-06-16 20:16:10 +0900

    #16 TOPIC実装

--- srcs/command/TOPIC.cpp
+++ srcs/command/TOPIC.cpp
@@ -1,38 +1,48 @@
-// #include "Command.hpp"
+#include "Command.hpp"
+#include "Channel.hpp"
 
-// void Command::TOPIC(Client &client, Server *server, const Message &message)
-// {
-// 	if(message.GetParams().size() < 1){
-// 		//paramが不足していたらエラー
-// 		return ;
-// 	}
+/* TOPICコマンドの処理を行う関数
+ * 引数1 -> クライアント
+ * 引数2 -> サーバーの情報
+ * 引数3 -> メッセージ */
+void Command::TOPIC(Client &client, Server *server, const Message &message)
+{
+	if(message.GetParams().size() < 1){
+		//paramが不足していたらエラー
+		return ;
+	}
 
-// 	std::string ch_name = message.GetParams()[0];
-// 	std::string topic;
-// 	std::string msg_to_c;
-// 	std::string msg_to_all;
+	std::string ch_name = message.GetParams()[0];
+	std::string topic;
+	std::string msg_to_c;
+	std::string msg_to_all;
 
-// 	Channel* ch = server->GetChannel(ch_name);
-// 	if(!ch){
-// 		msg_to_c = ERR_NOSUCHCHANNEL(client.GetNickname());
-// 		SendMessage(client.GetFd(), msg_to_c, 0);
-// 		return ;
-// 		// doesn't exist
-// 	}
-// 	else if(ch->GetPriv(client.GetUsername())  == P_None){
-// 		msg_to_c = ERR_NOTONCHANNEL(client.GetNickname(), ch_name);
-// 		SendMessage(client.GetFd(), msg_to_c, 0);
-// 		return ;
-// 		//you need oprator priv
-// 	}
-// 	if(message.GetParams().size() == 2){
-// 		topic = message.GetParams()[1];
-// 		ch->SetTopic(topic);
-// 		msg_to_all = client.GetNickname() + "! TOPIC " + ch_name + " :"+ topic;
-// 		ch->SendMsgToAll(msg_to_all);
-// 	}
-// 	else{
-// 		msg_to_all = client.GetNickname() + "! TOPIC " + ch_name + " :"+ ch->GetTopic();
-// 		ch->SendMsgToAll(msg_to_all);
-// 	}
-// }
+	Channel* ch = server->GetChannel(ch_name);
+
+	// チャンネルが存在しない場合
+	if(!ch){
+		msg_to_c = ERR_NOSUCHCHANNEL(client.GetNickname());
+		SendMessage(client.GetFd(), msg_to_c, 0);
+		return ;
+		// doesn't exist
+	}
+	// クライアントがチャンネルに参加していない場合
+	// else if(ch->GetPriv(client.GetUsername())  == P_None){
+	// 	msg_to_c = ERR_NOTONCHANNEL(client.GetNickname(), ch_name);
+	// 	SendMessage(client.GetFd(), msg_to_c, 0);
+	// 	return ;
+	// 	//you need oprator priv
+	// }
+	// チャンネルのトピックを設定
+	if(message.GetParams().size() == 2){
+		topic = message.GetParams()[1];
+		ch->SetToic(topic);
+		msg_to_all = client.GetNickname() + "! TOPIC " + ch_name + " :"+ topic;
+		ch->SendMsgToAll(msg_to_all);
+	}
+	// チャンネルのトピックを取得
+	else{
+		msg_to_all = client.GetNickname() + "! TOPIC " + ch_name + " :"+ ch->GetTopic();
+		ch->SendMsgToAll(msg_to_all);
+	}
+}
