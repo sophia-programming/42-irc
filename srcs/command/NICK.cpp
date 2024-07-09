04e469bf64d511a2a272898a3390237ac54c40a5
oaoba <hacchi0803.rera1126@gmail.com>
2024-07-09 14:17:21 +0900

    #32: [fix] Welcomeメッセージの条件

--- srcs/command/NICK.cpp
+++ srcs/command/NICK.cpp
@@ -1,7 +1,7 @@
 #include "Command.hpp"
 
 bool NickSize(std::string const &nickname);
-bool NickAlreadySet(std::string const &nickname, std::map<std::string, int> const &map_nick_fd);
+bool NickAlreadySet(std::string const &nickname, std::map<std::string, int> map_nick_fd);
 
 /* NICKコマンドの処理を行う関数
  * 引数1 -> クライアント
@@ -11,7 +11,7 @@ void Command::NICK(Client &client, std::map<std::string, int> &map_nick_fd, cons
 	const int &fd = client.GetFd();
 
 	//　引数がない場合　例）NICK
-	if (message.GetParams().size() < 1){
+	if (message.GetParams().size() < 1) {
 		SendMessage(fd, ERR_NONICKNAMEGIVEN, 0);
 		return ;
 	}
@@ -24,7 +24,7 @@ void Command::NICK(Client &client, std::map<std::string, int> &map_nick_fd, cons
 		SendMessage(fd, ERR_ERRONEUSNICKNAME(NewNick), 0);
 
 	// ニックネームがすでに設定されているかどうかを確認
-	else if (NickAlreadySet(NewNick, map_nick_fd) == true){
+	else if (NickAlreadySet(NewNick, map_nick_fd) == true) {
 		client.SetIsWelcome(false);
 		SendMessage(fd, ERR_NICKNAMEINUSE(OldNick, NewNick), 0);
 	}
@@ -36,20 +36,29 @@ void Command::NICK(Client &client, std::map<std::string, int> &map_nick_fd, cons
 
 		// ニックネームを設定
 		client.SetIsNick();
-		// Welcomeメッセージを送信
-		client.SetIsWelcome(true);
+
 		// ニックネームを設定
 		client.SetNickname(NewNick);
+
 		// もしクライアントが認証済みの場合
 		if (client.GetIsAuthenticated())
 			SendMessage(fd, RPL_NICK(OldNick, NewNick), 0);
+
+		// NICKとUSERコマンドの両方が設定されている場合, Welcomeメッセージを送信
+		if (client.GetIsNick() && client.GetIsUserSet()) {
+			client.SetIsWelcome(true);
+			SendMessage(fd, RPL_WELCOME(NewNick), 0);
+			SendMessage(fd, RPL_YOURHOST(NewNick), 0);
+			SendMessage(fd, RPL_CREATED(NewNick), 0);
+			SendMessage(fd, RPL_MYINFO(NewNick), 0);
+		}
 	}
 }
 
 /* ニックネームのサイズが9文字以下かどうかを確認する関数
  * 引数1 -> ニックネーム
  * 戻り値 -> ニックネームのサイズが9文字以下の場合はtrue, そうでない場合はfalse*/
-bool NickSize(std::string const &nickname){
+bool NickSize(std::string const &nickname) {
 	return (nickname.size() > 9) ? false : true;
 }
 
@@ -58,6 +67,6 @@ bool NickSize(std::string const &nickname){
  * 引数1 -> ニックネーム
  * 引数2 -> ニックネームとソケットファイルディスクリプタのマップ
  * 戻り値 -> ニックネームが設定されている場合はtrue, そうでない場合はfalse*/
-bool NickAlreadySet(std::string const &nickname, std::map<std::string, int> const &map_nick_fd){
-	return (map_nick_fd.find(nickname) != map_nick_fd.end());
+bool NickAlreadySet(std::string const &nickname, std::map<std::string, int> map_nick_fd) {
+	return map_nick_fd[nickname] > 0;
 }
