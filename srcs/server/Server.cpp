3362b38cdcd030f4c34cba01467d085fd5d376c8
kealoke <yuna.kealoke@gmail.com>
2024-07-17 19:07:42 +0900

    #27 [add] add this pointer at NICK called

--- srcs/server/Server.cpp
+++ srcs/server/Server.cpp
@@ -128,7 +128,7 @@ void Server::ExecuteCommand(int fd, const Message &message) {
 	}
 	// クライアントがニックネームを設定していない場合
 	else if (client.GetIsWelcome() == false && client.GetIsConnected() == false && cmd != "NICK") {
-		Command::NICK(client, map_nick_fd_, server_channels_, message);
+		Command::NICK(client, this, map_nick_fd_, server_channels_, message);
 		if (client.GetIsNick())
 			SendWelcomeMessage(client);
 		return;
