29e200e85323b9ac5830748896d7db4998a981cf
oaoba <hacchi0803.rera1126@gmail.com>
2024-07-28 16:01:22 +0900

    #35: [ok] clientが正しく切断され、メッセージが送信される

--- srcs/command/QUIT.cpp
+++ srcs/command/QUIT.cpp
@@ -14,69 +14,52 @@
 void Command::QUIT(Client &client, Server *server, std::vector<struct pollfd> &pollfds,
 				   std::map<int, Client> &users, std::map<std::string, int> &nick_to_fd,
 				   const std::vector<std::string> &params, const Message &message) {
-
 	const std::string &nick = client.GetNickname();
 	const int &clientFd = client.GetFd();
 
-	// もしQUITメッセージが指定されていない場合は、デフォルトのメッセージを使用
-	std::string quitMessage = "Goodbye";
-	if (!params.empty()) {
-		quitMessage = message.GetParams()[0];
-	}
+	// パラメータがない場合はデフォルトのメッセージ(Goodbye)を使用
+	std::string quitMessage = params.empty() ? "Goodbye" : params[0];
 
-	// クライアントが参加しているチャンネルのメンバーにのみQUITメッセージを送信
-	if (!server->GetChannels().empty()) {
-		const std::map<std::string, Channel*>& channels = server->GetChannels();
-		std::map<std::string, Channel*>::const_iterator it;
-		// debug
-		for (auto it = channels.begin(); it != channels.end(); ++it) {
-			std::cout << BOLD << "channels = " << it->first << STOP << std::endl;
-		}
+	// チャンネル内の他のメンバーにQUITメッセージを送信
+	const std::map<std::string, Channel*>& channels = server->GetChannels();
+	for (std::map<std::string, Channel*>::const_iterator it = channels.begin(); it != channels.end(); ++it) {
+		Channel* channel = it->second;
 
-		// チャンネルのメンバーにQUITメッセージを送信
-		for (it = channels.begin(); it != channels.end(); ++it) {
-			Channel* channel = it->second;
-			// 上記ポインタが入っているのを確認済み
+		// チャンネル内にクライアントがいるかどうかを確認
+		Client* channelMember = channel->GetUser(nick);
+		if (channelMember != NULL) {
+			std::cout << "Client " << nick << " is a member of channel " << channel->GetName() << std::endl;
 
-			//======================== ここから機能していない =======================
-
-			// チャンネルにクライアントが参加している場合
-			if (channel->IsMember(&client)) {
-				const std::vector<Client *> &members = channel->GetMember();
-				for (auto it = members.begin(); it != members.end(); ++it) {
-					std::cout << BOLD << (*it)->GetUsername() << STOP << std::endl;
+			// チャンネル内の他のメンバーにQUITメッセージを送信
+			std::string messageContent = QUIT_MESSAGE(nick, client.GetUsername(), client.GetHostname(), quitMessage);
+			for (std::map<Client*, User_Priv>::const_iterator member_it = channel->users_.begin(); member_it != channel->users_.end(); ++member_it) {
+				Client *member = member_it->first;
+				if (member->GetFd() != clientFd) {
+					SendMessage(member->GetFd(), messageContent, 0);
+					std::cout << "Sent QUIT message to member: " << member->GetNickname() << std::endl;
 				}
-
-				std::vector<Client *>::const_iterator member_it;
-				for (member_it = members.begin(); member_it != members.end(); ++member_it) {
-					Client *member = *member_it;
-
-					for (auto it = members.begin(); it != members.end(); ++it) {
-						std::cout << BOLD << (*it)->GetFd() << STOP << std::endl;
-					}
-					std::cout << BOLD << "clientFd " << clientFd << STOP << std::endl;
-
-//				 もしメンバーがクライアント自身でない場合は、QUITメッセージを送信
-					if (member->GetFd() != clientFd) {
-						std::cout << BOLD << "client.GetUsername() " << client.GetUsername() << STOP << std::endl;
-						std::string messageContent = QUIT_MESSAGE(nick, client.GetUsername(), client.GetHostname(), quitMessage);
-						SendMessage(member->GetFd(), messageContent, 0);
-					}
-
-	}
 			}
+
 			channel->RmUser(&client);
+			std::cout << "Removed client " << nick << " from channel " << channel->GetName() << std::endl;
+		} else {
+			std::cout << "Client " << nick << " is not a member of channel " << channel->GetName() << std::endl;
 		}
 	}
+
+	// クライアントにエラーメッセージを送信
+	std::string errorMessage = "ERROR :Closing Link: " + client.GetHostname() + " (" + quitMessage + ")\r\n";
+	SendMessage(clientFd, errorMessage, 0);
+	std::cout << "Sent error message to client: " << errorMessage << std::endl;
 
 	// クライアントの接続を終了し、関連するリソースをクリーンアップ
 	try {
 		ClearClientInfo(client, pollfds, users, nick_to_fd);
+		std::cout << "Cleared client info for: " << nick << std::endl;
 	} catch (const std::exception &e) {
 		std::cerr << "Error during client cleanup: " << e.what() << std::endl;
 	}
 
-	// サーバーからクライアントを削除
 	server->RemoveClient(clientFd);
+	std::cout << "Removed client from server: " << nick << std::endl;
 }
-
