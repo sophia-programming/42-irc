8f3ef0d45391e3c1b0395df62e6e5bd17ddf00ba
oaoba <hacchi0803.rera1126@gmail.com>
2024-07-30 22:11:02 +0900

    #35: [fix] チャンネルに入っていない場合,打った本人のみ接続遮断

--- srcs/command/QUIT.cpp
+++ srcs/command/QUIT.cpp
@@ -22,21 +22,24 @@ void Command::QUIT(Client &client, Server *server, std::vector<struct pollfd> &p
 	// パラメータがない場合はデフォルトのメッセージ(Goodbye)を使用
 	std::string quitMessage = params.empty() ? "Goodbye" : params[0];
 
+	// チャンネルに入っていない場合、QUITコマンドを打ったクライアントのみ接続遮断
+	if (server->GetChannels().empty()) {
+		ClearClientInfo(client, pollfds, users, nick_to_fd);
+		return;
+	}
+
 	// チャンネル内の他のメンバーにQUITメッセージを送信
 	const std::map<std::string, Channel*>& channels = server->GetChannels();
 	for (std::map<std::string, Channel*>::const_iterator it = channels.begin(); it != channels.end(); ++it) {
 		Channel* channel = it->second;
 
-		// チャンネル内にクライアントがいるかどうかを確認
-		Client* channelMember = channel->GetUser(nick);
-
-		// チャンネル内の他のメンバーにQUITメッセージを送信
 		std::string messageContent = QUIT_MESSAGE(nick, username, hostname, quitMessage);
 		for (std::map<Client*, User_Priv>::const_iterator member_it = channel->users_.begin(); member_it != channel->users_.end(); ++member_it) {
 			Client *member = member_it->first;
 			if (member->GetFd() != clientFd)
 				SendMessage(member->GetFd(), messageContent, 0);
 		}
+		// チャンネルからユーザーを削除
 		channel->RmUser(&client);
 	}
 
