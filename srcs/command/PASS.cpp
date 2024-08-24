1ca2f0660324ab82a11858eb05761771d0d67e2e
Yuna Shimomura <yushimom@c1r4s1.42tokyo.jp>
2024-08-24 22:48:08 +0900

    [fix] pass command and if else in executecommand function

--- srcs/command/PASS.cpp
+++ srcs/command/PASS.cpp
@@ -4,22 +4,25 @@
  * 引数1 -> クライアント
  * 引数2 -> サーバーパスワード
  * 引数3 -> メッセージ */
-void Command::PASS(Client &client, const std::string &server_password, const Message &message) {
+void Command::PASS(Client &client,const std::string &server_password, const Message &message) {
 	int const &fd = client.GetFd();
 	const std::string &nick = client.GetNickname();
+	std::vector<std::string> msg = message.GetParams();
 
 	// パラメータが1つでない場合
-	if (message.GetParams().size() != 1){
+	if (msg.size() != 1){
 		SendMessage(fd, ERR_NEEDMOREPARAMS(nick, "PASS"), 0);
 		return ;
 	}
 
 	// パスワードを取得
-	std::string const &password = message.GetParams()[0];
+	std::string const &password = RmRFromString(msg[0]);
 
 	// パスワードが正しい場合
 	if (password == server_password) {
 		client.SetIsAuthenticated(); // 認証済みに設定
+	}else{
+		SendMessage(client.GetFd(),ERR_PASSWDMISMATCH(client.GetNickname()),0);
 	}
 }
 
